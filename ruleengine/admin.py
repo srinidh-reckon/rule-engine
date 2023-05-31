9384a5f3b4712d1688a97837ff3096fffb7e0462
srinidh <srinidh@reckonsys.com>
2023-05-31 16:50:23 +0530

    admin list display

--- ruleengine/admin.py
+++ ruleengine/admin.py
@@ -11,13 +11,14 @@ class GenericRuleAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         # rule_engine.Rule(obj)  # Apply Rule on the object
         super().save_model(request, obj, form, change)  # Save the model
-
+    list_display = ['id','name','rule_des','left_atr','operator','right_atr','rule_string']
 admin.site.register(GenericRule, GenericRuleAdmin)
 
 class RuleAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         # rule_engine.Rule(obj)  # Apply Rule on the object
         super().save_model(request, obj, form, change)  # Save the model
+    list_display = ['id','name','des','rule_string']
 
 admin.site.register(Rule, RuleAdmin)
 
